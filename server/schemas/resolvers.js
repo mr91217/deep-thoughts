0ec6ba0fdffff1157d9eef2abb760a4725d522f9
ChunYu Chang <mr91217@gmail.com>
2021-10-14 16:42:37 -0400

    init files

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,15 @@
+const { User, Thought } = require('../models');
+
+
+
+
+const resolvers = {
+    Query: {
+        thoughts: async (parent, { username }) => {
+            const params = username ? { username } : {};
+            return Thought.find(params).sort({ createdAt: -1 });
+        },
+    }
+};
+  
+module.exports = resolvers;
