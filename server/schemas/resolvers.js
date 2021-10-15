b82b75c82e0e337dd2e938e1a88b81c45a8d1465
ChunYu Chang <mr91217@gmail.com>
2021-10-15 15:08:04 -0400

    build TD and resolver

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,10 +5,25 @@ const { User, Thought } = require('../models');
 
 const resolvers = {
     Query: {
-        thoughts: async (parent, { username }) => {
-            const params = username ? { username } : {};
-            return Thought.find(params).sort({ createdAt: -1 });
-        },
+      users: async () => {
+        return User.find()
+          .select('-__v -password')
+          .populate('thoughts')
+          .populate('friends');
+      },
+      user: async (parent, { username }) => {
+        return User.findOne({ username })
+          .select('-__v -password')
+          .populate('friends')
+          .populate('thoughts');
+      },
+      thoughts: async (parent, { username }) => {
+        const params = username ? { username } : {};
+        return Thought.find(params).sort({ createdAt: -1 });
+      },
+      thought: async (parent, { _id }) => {
+        return Thought.findOne({ _id });
+      }
     }
 };
   
